7c4acb8837be34e7a6cde6c2648e7d1f9513d3c4
The Medic <rein.reinstein@tktk.ee>
2024-09-26 18:39:00 +0300

    create new app

---
+++ Programeerimis_alused/Program.cs
@@ -0,0 +1,13 @@
+﻿//This is a comment to my code
+
+//rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");//output
+//string-sõne
+string username = Console.ReadLine();//input
+
+Console.WriteLine("Hello, " + username + "!");//output
+//string interpolation
+
+Console.WriteLine($"Hello, {username}!");//output
